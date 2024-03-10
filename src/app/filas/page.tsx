15304f4e123cf80f98fd96504e7325e857cdc55e
Thales Colovini <thales.soul96@gmail.com>
2024-03-10 15:55:15 -0300

    configura shadcn, e rotas basicas

---
+++ src/app/filas/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Queue() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <h1>Filas</h1>
+    </main>
+  );
+}
---
+++ src/app/incontingencia/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Incontigencia() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <h1>Incontigencias</h1>
+    </main>
+  );
+}
